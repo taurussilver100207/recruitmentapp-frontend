7815ef2d2f2cba1b7dc5dce49b4f4fe7fd69e4ef
taurussilver100207 <110682916+taurussilver100207@users.noreply.github.com>
2024-06-04 21:00:37 +0700

    Register Page

--- src/pages/authPage/index.jsx
+++ src/pages/authPage/index.jsx
@@ -3,7 +3,13 @@ import Form from "./Form"
 const AuthPage = () => {
     return (
         <>
-            <Form></Form>
+        <div className="min-h-screen pt-[45px] bg-slate-100" >
+          <div className="container mx-auto">
+            <div className="flex flex-col lg:flex-row w-[75%] bg-white rounded-xl mx-auto shadow-lg overflow-hidden">
+              <Form/>
+            </div>
+          </div>
+        </div>
         </>
     )
 }
