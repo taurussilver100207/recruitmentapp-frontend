302ee5df0a7c0c0362d376edfeabad1da2c99670
taurussilver100207 <110682916+taurussilver100207@users.noreply.github.com>
2024-06-04 21:27:06 +0700

    Big Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { useState } from 'react'
-=======
->>>>>>> origin/Candidate
-// import reactLogo from './assets/react.svg'
 import './App.css'
 import Recruiment from './Components/Recruiment/Recruiment.jsx'
 import { Routes, Route } from "react-router-dom";
@@ -15,6 +10,10 @@ import RecruimentRoundDetail from './Components/RecruimentRound/RecruimentRoundD
 import Home from './Components1/Home.jsx';
 import AdminRecuiment from './Components1/AdminRecuiment.jsx';
 import RecruimentRoundList from './Components/RecruimentRound/RecruimentRoundList.jsx';
+import SendMailForm from './Components/MailPage/sendMail.jsx'
+import ListJobs from './Components/JobPage/jobListAdmin.jsx';
+import UserJobList from './Components/JobPage/jobListUser.jsx';
+import CreateJobForm from './Components/JobPage/createJob.jsx';
 function App() {
 
   return (
@@ -29,34 +28,15 @@ function App() {
       <Route path='/recruimentRoundForm' element={<RecruimentRoundForm />} />
       <Route path='/candidateList' element={<CandidateList />} />
       <Route path='/candidateForm' element={<CandidateForm />} />
+      <Route path="/sendMail" element={<SendMailForm />} />
+      <Route path="/job-admin" element={<ListJobs />} />
+      <Route path="/job-user" element={<UserJobList />} />
+      <Route path="/createJob" element={<CreateJobForm />} />
     </Routes>
 
   )
-=======
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import SendMailForm from './Components/MailPage/sendMail.jsx'
-import ListJobs from './Components/JobPage/jobListAdmin.jsx';
-import UserJobList from './Components/JobPage/jobListUser.jsx';
-import './App.css';
-import CreateJobForm from './Components/JobPage/createJob.jsx';
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/sendMail" element={<SendMailForm />} />
-        <Route path="/job-admin" element={<ListJobs />} />
-        <Route path="/job-user" element={<UserJobList />} />
-        <Route path="/createJob" element={<CreateJobForm />} />
-      </Routes>
-    </BrowserRouter>
-  );
->>>>>>> origin/hoang
 }
-<<<<<<< HEAD
+
 
 export default App;
-=======
-export default App
->>>>>>> origin/Candidate
+
