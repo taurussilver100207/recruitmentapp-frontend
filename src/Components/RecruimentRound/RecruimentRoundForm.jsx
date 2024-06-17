6027452766327908c9e3510d5510fbae05cf94b9
ngocKhime <Phamngockhiem2004@gmail.com>
2024-06-17 22:54:50 +0700

    fisrtCommit recruiment

--- src/Components/RecruimentRound/RecruimentRoundForm.jsx
+++ src/Components/RecruimentRound/RecruimentRoundForm.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import {  useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './Recruiment.css'
 
--- src/Components1/Home.jsx
+++ src/Components1/Home.jsx
@@ -1,28 +1,33 @@
-import { NavLink } from 'react-router-dom'
 import './style.css'
+import {
+    Disclosure,
+    DisclosureButton,
+    DisclosurePanel,
+    Menu,
+    MenuButton,
+    MenuItem,
+    MenuItems,
+    Transition,
+} from '@headlessui/react'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+const navigation = [
+    { name: 'Dashboard', href: '#', current: true },
+    { name: 'Team', href: '#', current: false },
+    { name: 'Projects', href: '#', current: false },
+    { name: 'Calendar', href: '#', current: false },
+]
+
+function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+}
 export default function Home() {
-    
+    const handleClickSubmit = (event) => {
+        event.preventDefault()
+    }
+
     return (
         <div>
-            <>
-                {/* <div className="Auth-form-container">
-                    <form className="Auth-form">
-                        <div className="Auth-form-content">
-                            <h1 className='headingAuthForm'>Recruiment-App</h1>
-                            <br></br>
-                            <div className="list-group">
-                                <NavLink to="/listTestForm" className="list-group-item list-group-item-action " aria-current="true">
-                                    list Test Form
-                                </NavLink>
-                                <br></br>
-                                <NavLink to="/recruimentRoundForm" className="list-group-item list-group-item-action">recruimentRoundForm</NavLink>
-                                <br></br>
-                                <NavLink to="/recruimentRoundlist" className="list-group-item list-group-item-action">Recruiment Round List </NavLink>
-                            </div>
-                        </div>
-                    </form>
-                </div> */}
-            </>
+
             <Disclosure as="nav" className="bg-gray-800">
                 {({ open }) => (
                     <>
